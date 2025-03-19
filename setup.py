2e6e2a2d3a8f44e3190df1febd5eba535a83796c
Manav_Sheth <manavsheth112@gmail.com>
2025-03-18 20:24:29 -0400

    inclass09

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/m112-s/M09_Incalss#',
+    license='MIT',
+    packages=['demo']
+)
